4fe54b29ae5e2d754cd96757ed7db8448732213e
K.S.W <52404779+kimseongwoo61@users.noreply.github.com>
2022-03-08 21:03:20 +0900

    Delete pcapParser directory

--- pcapParser/infoTime.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-typedef struct _timeval
-{
-    long    tv_sec;         /* seconds*/
-    long    tv_usec;        /*andmicroseconds */
-
-}timeval;
--- pcapParser/packetHeader.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "infoTime.c"
-
-typedef struct _pcap_header
-{
-    timeval ts;    /*time stamp */
-    unsigned caplen; /* length of portionpresent */
-    unsigned len;     /*length thispacket (off wire) */
-}pcap_header;
